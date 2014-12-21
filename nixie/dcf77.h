cf8e63825675a15c2c539de85e1f5ee19b88c606
Marius <git@mgraefe.de>
2014-12-21 13:56:32 +0100

    added functions to header

--- nixie/dcf77.h
+++ nixie/dcf77.h
@@ -6,8 +6,15 @@
 #ifndef DCF77_H_
 #define DCF77_H_
 
+#include <stdint.h>
 
+struct time_t;
 
-
+void dcf77_init(struct time_t *time);
+void dcf77_scrap();
+void dcf77_add_bit(uint8_t bit);
+void dcf77_on_rising_flank(uint32_t time);
+void dcf77_on_falling_flank(uint32_t time);
+void dcf77_update(uint8_t signal, uint32_t time);
 
 #endif /* DCF77_H_ */
